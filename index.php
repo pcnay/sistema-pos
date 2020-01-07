7ef6bd5fad1904ad5696a02845a9a5427c2c172b
Ramon <pcnay2003@gmail.com>
2020-01-07 14:06:46 -0800

    Se baja el gestor de vistas de AdminLTE y se configura para el MVC

--- index.php
+++ index.php
@@ -1,3 +1,72 @@
 <?php
-	echo "Ejemplo";
+/* 
+PLANTILLA ADMINLTE:
+https://www.adminlte.io/
+
+Es una plantilla especializada en Dash Board, para mostrar contenidos para un sistema informatico, es gratuita.
+Esta contruida con: 
++ HTML 5
++ CSS 3
++ BOOTSTRAP 3
++ JQUERY 3
++ JQUERY UI 1.11.4
++ MULTIPLES PLUGINS JAVASCRIPT Y CSS
+
+CARACTERISTICAS:
+	No se preocupa por la maquetación web y diseño responsivo
+	Se puede trabajar por módulos
+	Hay que identificar las clases que ayudan a interpretar la plantilla
+	Hay que identificar los plugins (javascript ycss) para reutilizarlos
+	Código abierto
+
+	Plug a Utilizar:
+	
+	Bootstrap (CSS y JavaScript)
+	Font Awesome (Iconos)
+	Ionicos
+	Theme Style AdminLTE
+	Skins AdminLTE
+	Morris Chart
+	Jvectormap
+	Data Ranger Picker
+	iCheck (Check Box )
+	JQuery Knob Chart (Graficos)
+	DataTables
+	Chart Set
+	InputMask (Mascaras de Entradas)
+	SweetAlert (Alertas suaves)
+	JQuery Number (Formatos numeros, precios)
+	AdminLTE App (JavaScript)
+	TCPDF (Para imprimir facturas)
+
+	MVC (Modelo - Vista - Controlador)
+	Es un tipo de diseño que separa en capas bien definidas el desarrollo de una aplicación.
+	Modelo = Encargado en la lógica de la aplicación.
+	Controlador = Encargado de gestionar las peticiones del usuario, procesarlas invocando al 
+	Modelo y mostrarlas al usuario a través de las Vistas.
+	Vistas = Son las responsables de mostrar al usuario el resultado que obtiene del Modelo a travpes del Controlador.
+	
+	
+	Vista (Usuario)
+		Vista -> Controlador
+
+	Controlador
+		Controlador -> Modelo
+			Controlador -> Vistas
+
+	Modelo
+		Modelo -> Controlador
+
+
+		.htaccess = Permite proteger las carpetas y las URL amigables.
+	En la carpeta de "Vistas" se copian algunas carpetas de AdminLTE para iniciar con el MVC
+	Estas carpetas son:
+	* bower-components
+	* dist
+	* plugins
+
+*/ 
+
 ?>
+ 
+
