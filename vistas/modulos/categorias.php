b73b93536bb5295098c0194e8c8df68d769f28ea
Ramon <pcnay2003@gmail.com>
2020-02-18 06:00:18 +0000

    Se termina de maquetar, la opciones del menu la pantalla inicial de la Categoria solamente

--- vistas/modulos/categorias.php
+++ vistas/modulos/categorias.php
@@ -3,12 +3,12 @@
     <!-- Content Header (Page header) -->
     <section class="content-header">
       <h1>
-        Administrar Categroias
+        Administrar Categorías
         <small>Panel De Control</small>
       </h1>
       <ol class="breadcrumb">
-        <li><a href="#"><i class="fa fa-dashboard"></i> Inicio</a></li>
-        <li class="active">Administrar Categorias</li>
+        <li><a href="inicio"><i class="fa fa-dashboard"></i> Inicio</a></li>
+        <li class="active">Administrar Categorías</li>
       </ol>
     </section>
 
@@ -17,25 +17,71 @@
 
       <!-- Default box -->
       <div class="box">
+        
         <div class="box-header with-border">
-          <h3 class="box-title">Title</h3>
+          <!-- Abre una ventana Modal, se define en la parte última del documento.-->
 
-          <div class="box-tools pull-right">
-            <button type="button" class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip"
-                    title="Collapse">
-              <i class="fa fa-minus"></i></button>
-            <button type="button" class="btn btn-box-tool" data-widget="remove" data-toggle="tooltip" title="Remove">
-              <i class="fa fa-times"></i></button>
-          </div>
+          <button class="btn btn-primary"  data-toggle="modal" data-target="#modalAgregarCategoria">
+            Agregar Categorias
+          </button>       
         </div>
+ 
         <div class="box-body">
-          Start creating your amazing application!
-        </div>
+          <!-- Cuerpo de la ventana, donde se encuentran los datos, tablas, se utilizara tDAtaTable de Bootstrap esta completa, contiene buscar, paginador, ordenar las columnas  -->
+          <!-- Esta clases de "table" son del plugin "bootstrap"-->
+          <!-- "tabla" = Es para enlazarlo con DataTable, se utiliza el archivo  /frontend/vistas/js/plantilla.js-->
+          <table class="table table-bordered tabe-striped dt-responsive tablas">
+            <thead>
+              <tr>
+                <th style="width:10px">#</th>
+                <th>Categorias</th>
+                <th>Acciones </th>
+              </tr>
+            </thead>
+            <!-- Cuerpo de la Tabla -->
+            <tbody>
+              <tr>
+                <td>1</td>
+                <td>EQUIPO ELECTROMECANICO</td>
+                
+								<td>
+                  <div class="btn-group">
+                    <button class="btn btn-warning"><i class="fa fa-pencil"></i></button>
+                    <button class="btn btn-danger"><i class="fa fa-times"></i></button>
+                  </div>
+                </td>
+              </tr>
+              <tr>
+                <td>2</td>
+                <td>TALADROS</td>
+                
+								<td>
+                  <div class="btn-group">
+                    <button class="btn btn-warning"><i class="fa fa-pencil"></i></button>
+                    <button class="btn btn-danger"><i class="fa fa-times"></i></button>
+                  </div>
+                </td>
+              </tr>
+
+              <tr>
+                <td>3</td>
+                <td>ANDAMIOS</td>
+                
+								<td>
+                  <div class="btn-group">
+                    <button class="btn btn-warning"><i class="fa fa-pencil"></i></button>
+                    <button class="btn btn-danger"><i class="fa fa-times"></i></button>
+                  </div>
+                </td>
+              </tr>
+
+            </tbody>
+
+          </table> <!-- <table class="table table-bordered tabe-striped"> -->
+
+        </div> <!-- <div class="box-body"> -->
+
         <!-- /.box-body -->
-        <div class="box-footer">
-          Footer
-        </div>
-        <!-- /.box-footer-->
       </div>
       <!-- /.box -->
 
@@ -43,3 +89,54 @@
     <!-- /.content -->
   </div>
   <!-- /.content-wrapper -->
+
+
+<!--Este código se tomo desde el bootstrap - > Table 
+Cuando el usuario oprima el boton de "Agregar Categoria" se activa esta ventana.
+-->
+
+<!-- Modal -->
+<div id="modalAgregarCategoria" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+
+    <!-- Modal content-->
+    <div class="modal-content">
+
+      <!-- enctype= "multipart/form-data = Para subir archivos., se suprime -->
+      <form role="form" method="post">
+    
+        <!-- La franja azul de la ventana modal -->
+        <div class="modal-header" style= "background:#3c8dbc; color:white">
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+          <h4 class="modal-title">Agregar Categoria</h4>
+        </div>
+
+
+        <div class="modal-body">
+          <div class="box-body">
+            <!-- Clases de BootStrap para las formularios-->
+            <div class="form-group">
+              <div class = "input-group">
+                <span class="input-group-addon"><i class="fa fa-th"></i></span>
+                <input type="text" class="form-control input-lg" name="nuevaCategoria" placeholder = "Ingresar Categoria" required>
+              </div> <!-- <div class = "input-group"> -->           
+
+            </div> <!-- <div class="form-group"> -->
+
+          </div> <!-- <div class="box-body"> -->
+
+        </div> <!-- <div class="modal-body"> -->
+
+					<!-- Pie Del Modal-->
+          <div class="modal-footer">
+            <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Salir</button>
+            <button type="submit" class="btn btn-primary">Guardar Categoria</button>
+          </div>
+
+      </form>
+
+    </div> <!-- <div class="modal-content"> -->
+
+  </div> <!-- <div class="modal-dialog"> -->
+
+</div> <!-- <div id="modalAgregarUsuario" class="modal fade" role="dialog"> --> 
