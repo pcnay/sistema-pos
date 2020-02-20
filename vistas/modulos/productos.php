ac366e18717536f6e4588bf65443a3d9771a6070
Ramon <pcnay2003@gmail.com>
2020-02-20 05:55:20 +0000

    Se inicia con la maquetacion de la pantalla principal de los productos.

--- vistas/modulos/productos.php
+++ vistas/modulos/productos.php
@@ -7,7 +7,7 @@
         <small>Panel De Control</small>
       </h1>
       <ol class="breadcrumb">
-        <li><a href="#"><i class="fa fa-dashboard"></i> Inicio</a></li>
+        <li><a href="inicio"><i class="fa fa-dashboard"></i> Inicio</a></li>
         <li class="active">Administrar Productos</li>
       </ol>
     </section>
@@ -17,25 +17,99 @@
 
       <!-- Default box -->
       <div class="box">
+        
         <div class="box-header with-border">
-          <h3 class="box-title">Title</h3>
+          <!-- Abre una ventana Modal, se define en la parte última del documento.-->
 
-          <div class="box-tools pull-right">
-            <button type="button" class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip"
-                    title="Collapse">
-              <i class="fa fa-minus"></i></button>
-            <button type="button" class="btn btn-box-tool" data-widget="remove" data-toggle="tooltip" title="Remove">
-              <i class="fa fa-times"></i></button>
-          </div>
+          <button class="btn btn-primary"  data-toggle="modal" data-target="#modalAgregarProducto">
+            Agregar Producto 
+          </button>       
         </div>
+ 
         <div class="box-body">
-          Start creating your amazing application!
-        </div>
+          <!-- Cuerpo de la ventana, donde se encuentran los datos, tablas, se utilizara tDAtaTable de Bootstrap esta completa, contiene buscar, paginador, ordenar las columnas  -->
+          <!-- Esta clases de "table" son del plugin "bootstrap"-->
+          <!-- "tabla" = Es para enlazarlo con DataTable, se utiliza el archivo  /frontend/vistas/js/plantilla.js-->
+          <table class="table table-bordered tabe-striped dt-responsive tablas">
+            <thead>
+              <tr>
+                <th style="width:10px">#</th>
+                <th>Imagen</th>
+                <th>Codigo</th>
+                <th>Descripcion</th>
+                <th>Categoria</th>
+                <th>Stock</th>
+                <th>Precio Compra</th>
+								<th>Precio Venta</th>
+								<th>Agregado</th>
+                <th>Acciones </th>
+              </tr>
+            </thead>
+            <!-- Cuerpo de la Tabla -->
+            <tbody>
+              <tr>
+                <td>1</td>
+                <td><img src="vistas/img/productos/default/anonymous.png" class="img-thumbnail" width="40px"></td>
+                <td>0001</td>
+                <!-- Clase de BootStrap -->
+                <td>Descripcion del Producto</td>
+                <td>Descripcion Categoria</td>
+                <td>20</td>
+								<td>156.40</td>
+								<td>250.40</td>
+                <td>2020-01-11 12:05:32</td>
+                <td>
+                  <div class="btn-group">
+                    <button class="btn btn-warning"><i class="fa fa-pencil"></i></button>
+                    <button class="btn btn-danger"><i class="fa fa-times"></i></button>
+                  </div>
+                </td>
+              </tr>
+
+              <tr>
+                <td>2</td>
+                <td><img src="vistas/img/productos/default/anonymous.png" class="img-thumbnail" width="40px"></td>
+                <td>0001</td>
+                <!-- Clase de BootStrap -->
+                <td>Descripcion del Producto</td>
+                <td>Descripcion Categoria</td>
+                <td>20</td>
+								<td>156.40</td>
+								<td>250.40</td>
+                <td>2020-01-11 12:05:32</td>
+                <td>
+                  <div class="btn-group">
+                    <button class="btn btn-warning"><i class="fa fa-pencil"></i></button>
+                    <button class="btn btn-danger"><i class="fa fa-times"></i></button>
+                  </div>
+                </td>
+              </tr>
+
+              <tr>
+                <td>3</td>
+                <td><img src="vistas/img/productos/default/anonymous.png" class="img-thumbnail" width="40px"></td>
+                <td>0001</td>
+                <!-- Clase de BootStrap -->
+                <td>Descripcion del Producto</td>
+                <td>Descripcion Categoria</td>
+                <td>20</td>
+								<td>156.40</td>
+								<td>250.40</td>
+                <td>2020-01-11 12:05:32</td>
+                <td>
+                  <div class="btn-group">
+                    <button class="btn btn-warning"><i class="fa fa-pencil"></i></button>
+                    <button class="btn btn-danger"><i class="fa fa-times"></i></button>
+                  </div>
+                </td>
+              </tr>
+
+            </tbody>
+
+          </table> <!-- <table class="table table-bordered tabe-striped"> -->
+        </div> <!-- <div class="box-body"> -->
+
         <!-- /.box-body -->
-        <div class="box-footer">
-          Footer
-        </div>
-        <!-- /.box-footer-->
       </div>
       <!-- /.box -->
 
@@ -43,3 +117,86 @@
     <!-- /.content -->
   </div>
   <!-- /.content-wrapper -->
+
+
+<!--Este código se tomo desde el bootstrap - > Table 
+Cuando el usuario oprima el boton de "Agregar Usuario" se activa esta ventana.
+-->
+
+<!-- Modal -->
+<div id="modalAgregarUsuario" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+
+    <!-- Modal content-->
+    <div class="modal-content">
+
+      <!-- enctype= "multipart/form-data = Para subir archivos. -->
+      <form role="form" method="post" enctype= "multipart/form-data">
+    
+        <!-- La franja azul de la ventana modal -->
+        <div class="modal-header" style= "background:#3c8dbc; color:white">
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+          <h4 class="modal-title">Agregar Usuario</h4>
+        </div>
+
+
+        <div class="modal-body">
+          <div class="box-body">
+            <!-- Clases de BootStrap para las formularios-->
+            <div class="form-group">
+              <div class = "input-group">
+                <span class="input-group-addon"><i class="fa fa-user"></i></span>
+                <input type="text" class="form-control input-lg" name="nuevoNombre" placeholder = "Ingresar Nombre" required>
+              </div> <!-- <div class = "input-group"> -->           
+            </div> <!-- <div class="form-group"> -->
+
+            <div class="form-group">
+              <div class = "input-group">
+                <span class="input-group-addon"><i class="fa fa-key"></i></span>
+                <input type="text" class="form-control input-lg" name="nuevoUsuario" placeholder = "Ingresar Usuario" required>
+              </div> <!-- <div class = "input-group"> -->           
+            </div> <!-- <div class="form-group"> -->
+
+            <div class="form-group">
+              <div class = "input-group">
+                <span class="input-group-addon"><i class="fa fa-lock"></i></span>
+                <input type="password" class="form-control input-lg" name="nuevoPassword" placeholder = "Ingresar Contraseña" required>
+              </div> <!-- <div class = "input-group"> -->           
+            </div> <!-- <div class="form-group"> -->
+
+            <div class="form-group">
+              <div class = "input-group">
+                <span class="input-group-addon"><i class="fa fa-users"></i></span>
+                <select class="form-control input-lg" name="nuevoPerfil">
+                  <option value="">Seleccionar perfil</option>
+                  <option value="Administrador">Administrador</option>
+                  <option value="Especial">Especial</option>
+                  <option value="Vendedor">Vendedor</option>
+                </select>                
+              </div> <!-- <div class = "input-group"> -->           
+            </div> <!-- <div class="form-group"> -->
+
+            <div class="form-group">
+              <div class="panel text-up">SUBIR FOTO</div> 
+              <input type="file" id="nuevaFoto" name="nuevaFoto">
+              <p class="help-block">Peso Máximo de la foto 200 Mb</p>
+              <img src="vistas/img/usuarios/default/anonymous.png" class="img-thumbnail" width = "100px">
+
+            </div> <!-- <div class="form-group"> -->
+
+          </div> <!-- <div class="box-body"> -->
+
+        </div>
+
+          <div class="modal-footer">
+            <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Salir</button>
+            <button type="submit" class="btn btn-primary">Guardar Usuarios</button>
+          </div>
+
+        </form>
+
+    </div> <!-- <div class="modal-content"> -->
+
+  </div> <!-- <div class="modal-dialog"> -->
+
+</div> <!-- <div id="modalAgregarUsuario" class="modal fade" role="dialog"> -->
