9c91318b10406fbe8445fa99b3a5aad9c1df852a
Ramon <pcnay2003@gmail.com>
2020-03-16 21:17:28 +0000

    Se agrega el código para colocar color al Stock cuando esta por terminar o esta completo, se agrega propiedades para optimizar la carga de datos en la pantalla utilizando el pluging DataTable, y se agega las propiedades a los botones solamente.

--- ajax/datatable-productos.ajax.php
+++ ajax/datatable-productos.ajax.php
@@ -19,8 +19,6 @@
 			$productos = ControladorProductos::ctrMostrarProductos($item,$valor);
 
 			
-			// $imagen = "<img src='/vistas/img/productos/101/105.png' width='40px'>";
-			$botones = "<div class='btn-group'><button class='btn btn-warning'><i class='fa fa-pencil'></i></button><button class='btn btn-danger'><i class='fa fa-times'></i></button></div>";
 
 			//var_dump($productos);
 
@@ -28,14 +26,50 @@
 				"data": [';
 				for ($i =0;$i<count($productos);$i++)
 				{
+					// Se obtiene la imagen del producto, se pasa a variable para agregarlo al JSon.
 					$imagen = "<img src='".$productos[$i]["imagen"]."' width='40px'>";
+					// se extrae la categoria
+					$item = "id";
+					$valor = $productos[$i]["id_categoria"];
+					$categoria = ControladorCategorias::ctrMostrarCategorias($item,$valor);
+
+					// Se utilizara un color para determinar el "Stock" de los productos.
+					if ($productos[$i][stock] <= 10)
+					{
+						$stock = "<button class='btn btn-danger'>".$productos[$i][stock]."</button>";
+					}
+					else if ($productos[$i][stock] > 11 && $productos[$i][stock] <=15)
+					{
+						$stock = "<button class='btn btn-warning'>".$productos[$i][stock]."</button>";
+					}
+					else // if ($productos[$i][stock] <= 10)
+					{
+						$stock = "<button class='btn btn-success'>".$productos[$i][stock]."</button>";
+					}
+
+					
+
+					// $imagen = "<img src='/vistas/img/productos/101/105.png' width='40px'>";
+					// Se utilizan estos datos para pasarlos con Ajax a las funciones de JavaScript para obtener la información del "Producto" 
+					// se agrega btnEditarProducto" = Boton para editar 
+					// idProducto='".$productos[$i]["id"]."' para editar el producto.
+					// data-toggle='modal' = Para desplegar una ventanta Modal.
+					// data-target='#modalEditarProducto' = Pantalla para editar los productos 
+					// btnEliminarProducto= Boton para eliminar producto
+					// idProducto='".$productos[$i]["id"]."' = Para obtener el código del producto
+					// imagen='".$productos[$i]["imagen"]."' = Para obtener la ruta de la imagen.
+
+
+					// se extrae los datos utilizados para el boton de "Editar" y "Borrar"
+					$botones = "<div class='btn-group'><button class='btn btn-warning btnEditarProducto' idProducto='".$productos[$i]["id"]."' data-toggle='modal' data-target='#modalEditarProducto'><i class='fa fa-pencil'></i></button><button class='btn btn-danger btnEliminarProducto' idProducto='".$productos[$i]["id"]."' codigo ='".$productos[$i]["codigo"]."' imagen='".$productos[$i]["imagen"]."' ><i class='fa fa-times'></i></button></div>";
+
 					$datosJson  .= '[
 							"'.($i+1).'",
 							"'.$imagen.'",
 							"'.$productos[$i][codigo].'",
 							"'.$productos[$i][descripcion].'",
-							"Taladros", 
-							"'.$productos[$i][stock].'",
+							"'.$categoria["nombre"].'", 
+							"'.$stock.'",
 							"'.$productos[$i][precio_compra].'",
 							"'.$productos[$i][precio_venta].'",
 							"'.$productos[$i][fecha].'",							
--- vistas/js/productos.js
+++ vistas/js/productos.js
@@ -2,18 +2,48 @@
 
 // Verificar que los datos Json estan correctos.
 
+// En esta parte se agrega la tabla a la plugin "DataTable" y no quema en el HTML el contenido de los campos.
+// Se agregan tres propiedades últimas para mejorar el desempeño en la carga de la páginas.
+$('.tablaProductos').DataTable({
+	"ajax":"ajax/datatable-productos.ajax.php",
+	"defenderRender":true,
+	"retrieve":true,
+	"processing":true,
+  "language":{ 
+    "sProcessing": "Procesando ...",
+    "sLengthMenu": "Mostrar _MENU_ registros",
+    "sZeroRecords": "No se encontraron resultados",
+    "sEmptyTable": "Ningún dato disponible en esta tabla",
+    "sInfo": "Mostrando registros del _START_ al _END_ de un total de _TOTAL_",
+    "sInfoEmpty": "Mostrando registros del 0 al 0 de un total de 0",
+    "sInfoFiltered": "(filtrado de un total de _MAX_ registros",
+    "sInfoPostFix": "",
+    "sSearch": "Buscar",
+    "sUrl": "",
+    "sInfoThousands": ",",
+    "sLoadingRecords": "Cargando ...",
+    "oPaginate":{
+      "sFirst": "Primero",
+      "sLast": "Ultimo",
+      "sNext": "Siguiente",
+      "sPrevious": "Anterior",
+		},
+		"oAria": {
+      "sSortAscending": ": Activar para ordenar la columna de manera ascendente",
+			"sSortDescending": ": Activar para ordenar la columna de manera descendente"
+		},
+	}
+
+});
+
+/*
 $.ajax({		
 	url:"ajax/datatable-productos.ajax.php",
 	success:function(respuesta){
 	console.log("respuesta",respuesta);
 		}
 })
-
-// En esta parte se agrega la tabla a la plugin "DataTable" y no quema en el HTML el contenido de los campos.
-$('.tablaProductos').DataTable({
-	"ajax":"ajax/datatable-productos.ajax.php"
-})
-
+*/
 
 /*
 $('.tabla').DataTable({
@@ -21,9 +51,3 @@ $('.tabla').DataTable({
 });
 */ 
 
-/*
-$(document).ready(function(){
-	('#tablas').DataTable({
-		ajax:"data/ajax.txt"});
-});
-*/
