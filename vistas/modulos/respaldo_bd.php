48b68e1428fa0817731c7187212751da753353f0
Ramon <pcnay2003@gmail.com>
2020-09-06 19:16:51 +0000

    Se agrega el modulo para respaldar las bases de datos

---
+++ vistas/modulos/respaldo_bd.php
@@ -0,0 +1,36 @@
+<?php
+	echo "Iniciando el respaldo ... ";
+  $db_host = 'localhost';
+  $db_name = 'pos';
+  $db_user = 'ventas-pos';
+  $db_pass = 'pcnay2003';
+
+  $fecha = date("Ymd-His");
+  $salida_sql = $db_name.'-'.$fecha.'.sql';
+  $dump = "mysqldump -h$db_host -u$db_user -p$db_pass --opt $db_name > $salida_sql";
+  system($dump,$output);
+  // Se puede ejecutar la funcion : phpinfo() y buscar el comando “zip”, si no esta habilitadora en el archivo ( php.ini ) se descomenta.
+
+  $zip = new ZipArchive();
+  $salida_zip = $db_name.'-'.$fecha.'.zip';
+  if ($zip->open($salida_zip,ZIPARCHIVE::CREATE) == true)
+  {
+    $zip->addFile($salida_sql);
+    $zip->close();  
+    
+    // Para borrar el archivo .sql
+    //unlink ($salida_sql); 
+    //unlink ($salida_zip); 
+
+    // para que muestre como descarga desde un navegador.
+    //header ("Location: $salida_zip");
+    
+  }
+  else
+  {
+    echo "Error";
+  }
+
+
+?>
+  
